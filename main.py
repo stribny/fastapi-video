a83025f7ddf03438b934d7ad865cc9de78acd88c
Steven X. Han <steven@stevenhan.net>
2022-02-26 13:26:21 +1100

    Fix the bug where the video does not stream to the end in Chrome (Tested on Version 98.0.4758.109 (Official Build) (x86_64))

--- main.py
+++ main.py
@@ -20,13 +20,16 @@ async def read_root(request: Request):
 async def video_endpoint(range: str = Header(None)):
     start, end = range.replace("bytes=", "").split("-")
     start = int(start)
-    end = int(end) if end else start + CHUNK_SIZE
+    length = int(end) - start if end else CHUNK_SIZE
+    end = start + length - 1
     with open(video_path, "rb") as video:
         video.seek(start)
-        data = video.read(end - start)
-        filesize = str(video_path.stat().st_size)
+        data = video.read(length)
+        total = video_path.stat().st_size
+        if end > total - 1:
+            end = total - 1
         headers = {
-            'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
-            'Accept-Ranges': 'bytes'
+            "Content-Range": f"bytes {str(start)}-{str(end)}/{str(total)}",
+            "Accept-Ranges": "bytes"
         }
         return Response(data, status_code=206, headers=headers, media_type="video/mp4")
